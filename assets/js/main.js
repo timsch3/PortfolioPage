a78220dd1cc25a7f5714afce80ff0263148071eb
timsch3 <timsch3@gmail.com>
2021-10-05 18:48:23 +0200

    removed scrollPos var and instead use scrollY

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 let allSections = document.querySelectorAll('section')
 let activeSection = 0
-let scrollPos = 0
 let navOverlay = document.getElementById('nav-overlay')
 let navUp = document.getElementById('nav-up')
 let navDown = document.getElementById('nav-down')
@@ -8,25 +7,24 @@ let navDown = document.getElementById('nav-down')
 hideNav(navUp) // hide initally
 
 function setPos() { // set scroll position vars and show/hide navs 
-    scrollPos = window.pageYOffset
-    activeSection = Number((scrollPos / allSections[0].offsetHeight).toFixed(0))
+    activeSection = Number((scrollY / allSections[0].offsetHeight).toFixed(0))
     if (activeSection == 0) hideNav(navUp)
     else showNav(navUp)
     if (activeSection == allSections.length - 1) hideNav(navDown)
     else showNav(navDown)
-    console.log(activeSection)
+    console.log(scrollY)
+    console.log(window.pageYOffset)
 }
 
 function navigateUp() {
     if (activeSection > 0) {
-        window.scrollTo({ top: (scrollPos - allSections[activeSection - 1].offsetHeight), left: 0, behavior: 'smooth' })
+        window.scrollTo({ top: (scrollY - allSections[activeSection - 1].offsetHeight), left: 0, behavior: 'smooth' })
         activeSection--
     }
 }
 function navigateDown() {
     if (activeSection < allSections.length - 1) {
-        showNav(navUp)
-        window.scrollTo({ top: scrollPos + allSections[activeSection + 1].offsetHeight, left: 0, behavior: 'smooth' })
+        window.scrollTo({ top: scrollY + allSections[activeSection + 1].offsetHeight, left: 0, behavior: 'smooth' })
         activeSection++
     }
 }
